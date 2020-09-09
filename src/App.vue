9d531403996ce7b19246205c25e093bbceb5c434
frederik.steffen <frederik.steffen@gmail.com>
2020-09-09 15:55:09 +0200

    centered layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
+  <main id="app">
     <Countdown :end="end" />
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -21,12 +21,22 @@ export default class App extends Vue {
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  display: grid;
+}
+
+* {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
 }
 </style>
