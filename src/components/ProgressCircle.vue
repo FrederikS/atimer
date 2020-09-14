a527af83ffc63c40ad1507d953d545988350f210
fdk <frederik.steffen@gmail.com>
2020-09-14 14:53:48 +0200

    added title and made some layout adjustments (#7)

--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -21,7 +21,7 @@
         :cy="radius"
       />
     </svg>
-    <div class="info" :style="style">
+    <div class="info">
       <slot />
     </div>
   </div>
@@ -45,10 +45,6 @@ export default class ProgressCircle extends Vue {
 
   get offset(): number {
     return this.circumference - (this.percentage / 100) * this.circumference;
-  }
-
-  get style(): string {
-    return `padding: ${this.radius / 2}px`;
   }
 }
 </script>
